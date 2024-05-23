d566d43332912ffb637c44fa79ef38d727b7434b
arturo <arturo.veliz@ucb.edu.bo>
2024-05-23 14:19:02 -0400

    Se elimino codigo sin usar en birthdayService

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -14,22 +14,4 @@ export class BirthdayService {
     });
 
   }
-
-  SendMessageBirthday(employees,smtpUrl, smtpPort, transport)
-  {
-      employees.forEach((employee) => {
-          const message = {
-            host: smtpUrl,
-            port: smtpPort,
-            from: "sender@here.com",
-            to: [employee.getEmail()],
-            subject: "Happy Birthday!",
-            text: `Happy Birthday, dear ${employee.getFirstName()}!`,
-          };
-          transport.sendMail(message);
-        });
-  }
-
-
-  
 }
